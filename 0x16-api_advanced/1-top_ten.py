4cd59efbf36008b93eab17da4ebf3c50634273eb
ysamwel <yvonneatieno2000@gmail.com>
2024-06-05 01:47:44 +0300

    fixes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     '''returns the top ten posts for a given subreddit'''
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-            .format(subreddit), headers=user).json()
+                       .format(subreddit), headers=user).json()
     try:
         for post in url.get('data').get('children'):
             print(post.get('data').get('title'))
