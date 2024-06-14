327b6949ac5f4b7640adb4c8db83983f6821c0da
ysamwel <yvonneatieno2000@gmail.com>
2024-06-14 05:50:51 +0300

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,14 @@
+# this script Changes the OS configuration
+so that it is possible to login with the
+# holberton user and open a file without any error message.
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
