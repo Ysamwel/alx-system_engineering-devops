371c75852b1edb4e1ade0a7126bafd13f7800ac6
ysamwel <yvonneatieno2000@gmail.com>
2024-06-14 04:46:14 +0300

    fixing nginx to serve more requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,21 @@
-# fixing nginx to enable acceptance
-and serve more requests
+# Ensure the file line contains the correct limit
+file_line { 'set_nginx_open_files_limit':
+  path  => '/etc/default/nginx',
+  line  => 'NGINX_ULIMIT=10000',
+  match => '^NGINX_ULIMIT=',
+  notify => Service['nginx'],  # Notify Nginx service if the line changes
+}
 
-exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+# Ensure Nginx service is restarted if necessary
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  subscribe => File_line['set_nginx_open_files_limit'],  # Restart Nginx if the configuration changes
+}
+
+# Ensure the system-wide limits are properly set
+exec { 'increase_system_open_files_limit':
+  command => 'echo "* soft nofile 10000\n* hard nofile 10000" >> /etc/security/limits.conf',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => 'grep -q "10000" /etc/security/limits.conf',  # Only run if not already set
 }
